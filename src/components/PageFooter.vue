47da2a73ee0b8af84b8f82e926e70a3ea3835af8
eternityspring@163.com <1526qaws>
2017-09-16 21:49:35 +0800

    init

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="footer">
+    <div class="header-lines-decoration">
+      <span class="bg-secondary-color"></span>
+      <span class="bg-blue"></span>
+      <span class="bg-blue-light"></span>
+      <span class="bg-orange-light"></span>
+      <span class="bg-red"></span>
+      <span class="bg-green"></span>
+      <span class="bg-secondary-color"></span>
+    </div>
+    <div class="container content">
+      <div class="row">
+        <div class="col s4">
+          <div class="logo">
+            优燃+
+          </div>
+        </div>
+        <div class="col s8">
+          <div class="row">
+            <div class="col s6">
+              <ul>
+                <li>关于我们</li>
+                <li>联系我们</li>
+                <li>巴拉巴拉</li>
+              </ul>
+            </div>
+            <div class="col s6 qrcode">
+              <p>关注我们</p>
+              <img src="https://ninghao.net/files/images/blog/3040.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-info">
+      <p class="center-align">Copyright © uranplus.com<span>|</span>
+        <a href="/copyright">免责申明</a> <span>|</span>
+        <a href="/about">关于</a>
+        <span>|</span>鄂ICP备130666660号-1
+        <span>|</span>QQ咨询:
+        <a target="_bl/explorerank" href="" rel="nofollow">99989999</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageFooter',
+    data () {
+      return {
+        msg: '页脚啊'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
