b1566fba1140389e45cca9c67199c3dbf89b46b5
wuyunfeng <1363321437@qq.com>
2017-09-16 23:12:00 +0800

    update

--- src/pages/Front.vue
+++ src/pages/Front.vue
@@ -1,141 +1,141 @@
 <template>
-  <div class="front">
-    <div class="section-top">
-      <div class="container">
-        <div class="slogan center-align">
-          <div class="content">
-            <h1>一个很潮的团队，做着一些很潮的事情</h1>
-          </div>
+    <div class="front">
+        <div class="section-top">
+            <div class="container">
+                <div class="slogan center-align">
+                    <div class="content">
+                        <h1>一个很潮的团队，做着一些很潮的事情</h1>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="section-intro">
+            <div class="container center-align">
+                <div class="title">
+                    <h2>优燃+不止是代码工厂</h2>
+                    <p>不同于传统的客户以上帝视角给出需求开发方以工厂模式提供产品</p>
+                    <p>我们提供一整套解决方案，以解决客户痛点为乐趣</p>
+                </div>
+                <div class="row">
+                    <div class="col s4">
+                        <div class="icon-box">
+                            <span class="ran-aim"></span>
+                        </div>
+                        <p>从客户角度出发 深度了解客户需求 探讨应用场景</p>
+                    </div>
+                    <div class="col s4">
+                        <div class="icon-box">
+                            <span class="ran-microsoftword"></span>
+                        </div>
+                        <p>从需求出发 剖析客户痛点</p>
+                    </div>
+                    <div class="col s4">
+                        <div class="icon-box">
+                            <span class="ran-redux"></span>
+                        </div>
+                        <p>针对客户痛点 选用相对的技术和解决方案</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section-technology">
+            <div class="container center-align">
+                <div class="title">
+                    <h2>我们的技术栈</h2>
+                    <p>在保证服务质量的同时，探索新技术</p>
+                    <div class="center-align">各种牛逼的技术 我编不下去了</div>
+                </div>
+                <div class="row">
+                    <div class="col s3">
+                        <span class="ran-sass"></span>
+                        <p>sass</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-react"></span>
+                        <p>react</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-vuejs"></span>
+                        <p>vuejs</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-angular"></span>
+                        <p>angular</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-nodejs"></span>
+                        <p>nodejs</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-npm"></span>
+                        <p>npm</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-laravel"></span>
+                        <p>laravel</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-html5"></span>
+                        <p>html5</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-git"></span>
+                        <p>git</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-gulp"></span>
+                        <p>gulp</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-wordpress"></span>
+                        <p>wordpress</p>
+                    </div>
+                    <div class="col s3">
+                        <span class="ran-javascript"></span>
+                        <p>javascript</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section-intro">
+            <div class="container center-align">
+                <div class="title">
+                    <h2>我们的愿景</h2>
+                    <p>希望有一天赚大钱</p>
+                    <p>继续吹</p>
+                </div>
+                <div class="row">
+                    <div class="col s4">
+                        <div class="icon-box">
+                            <span class="ran-aim"></span>
+                        </div>
+                        <p>从客户角度出发 深度了解客户需求 探讨应用场景</p>
+                    </div>
+                    <div class="col s4">
+                        <div class="icon-box">
+                            <span class="ran-microsoftword"></span>
+                        </div>
+                        <p>从需求出发 剖析客户痛点</p>
+                    </div>
+                    <div class="col s4">
+                        <div class="icon-box">
+                            <span class="ran-redux"></span>
+                        </div>
+                        <p>针对客户痛点 选用相对的技术和解决方案</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="section-intro">
-      <div class="container center-align">
-        <div class="title">
-          <h2>优燃+不止是代码工厂</h2>
-          <p>不同于传统的客户以上帝视角给出需求开发方以工厂模式提供产品</p>
-          <p>我们提供一整套解决方案，以解决客户痛点为乐趣</p>
-        </div>
-        <div class="row">
-          <div class="col s4">
-            <div class="icon-box">
-              <span class="ran-aim"></span>
-            </div>
-            <p>从客户角度出发 深度了解客户需求 探讨应用场景</p>
-          </div>
-          <div class="col s4">
-            <div class="icon-box">
-              <span class="ran-microsoftword"></span>
-            </div>
-            <p>从需求出发 剖析客户痛点</p>
-          </div>
-          <div class="col s4">
-            <div class="icon-box">
-              <span class="ran-redux"></span>
-            </div>
-            <p>针对客户痛点 选用相对的技术和解决方案</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="section-technology">
-      <div class="container center-align">
-        <div class="title">
-          <h2>我们的技术栈</h2>
-          <p>在保证服务质量的同时，探索新技术</p>
-          <div class="center-align">各种牛逼的技术  我编不下去了</div>
-        </div>
-        <div class="row">
-          <div class="col s3">
-            <span class="ran-sass"></span>
-            <p>sass</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-vuejs"></span>
-            <p>vuejs</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-angular"></span>
-            <p>angular</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-nodejs"></span>
-            <p>nodejs</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-npm"></span>
-            <p>npm</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-laravel"></span>
-            <p>laravel</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-html5"></span>
-            <p>html5</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-git"></span>
-            <p>git</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-gulp"></span>
-            <p>gulp</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-react"></span>
-            <p>react</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-wordpress"></span>
-            <p>wordpress</p>
-          </div>
-          <div class="col s3">
-            <span class="ran-javascript"></span>
-            <p>javascript</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="section-intro">
-      <div class="container center-align">
-        <div class="title">
-          <h2>我们的愿景</h2>
-          <p>希望有一天赚大钱</p>
-          <p>继续吹</p>
-        </div>
-        <div class="row">
-          <div class="col s4">
-            <div class="icon-box">
-              <span class="ran-aim"></span>
-            </div>
-            <p>从客户角度出发 深度了解客户需求 探讨应用场景</p>
-          </div>
-          <div class="col s4">
-            <div class="icon-box">
-              <span class="ran-microsoftword"></span>
-            </div>
-            <p>从需求出发 剖析客户痛点</p>
-          </div>
-          <div class="col s4">
-            <div class="icon-box">
-              <span class="ran-redux"></span>
-            </div>
-            <p>针对客户痛点 选用相对的技术和解决方案</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
-
 <script>
-  export default {
+export default {
     name: 'Front',
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
     }
-  }
+}
+
 </script>
